fc473d35640040cb96fc977ee14b6fb28e9ef0e4
ndukwesamuel <ndukwesamuel23@gmail.com>
2024-08-23 04:03:02 -0700

    feat:add

--- screens/Vendor/Home/UploadProduct.js
+++ screens/Vendor/Home/UploadProduct.js
@@ -7,6 +7,8 @@ import {
   ScrollView,
   TouchableOpacity,
   Image,
+  FlatList,
+  Modal,
 } from "react-native";
 import React, { useState } from "react";
 import AppScreenTwo from "../../../components/shared/AppScreenTwo";
@@ -59,88 +61,204 @@ const UploadProduct = () => {
     }
   };
 
+  const [selectedStatus, setSelectedStatus] = useState("anniversary");
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const statuses = [
+    "anniversary",
+    "birthday",
+    "wedding",
+    "customized",
+    "cupcake",
+  ];
+
+  const handleStatusSelect = (status) => {
+    setSelectedStatus(status);
+    setIsModalVisible(false);
+  };
+
   return (
     <AppScreenTwo arrrow={"true"}>
-      <ScrollView>
-        <View style={styles.container}>
-          <Text style={{ fontSize: 32, fontWeight: "700" }}>Add Cake</Text>
-          <View style={{ flexDirection: "column", gap: 50 }}>
-            <View style={{ marginTop: 10, gap: 15 }}>
-              <View style={styles.formGroup}>
-                <Text style={styles.label}>Name of Cake</Text>
-                <TextInput
-                  style={styles.input}
-                  value={cakeName}
-                  onChangeText={(text) => setCakeName(text)}
-                />
-              </View>
-              <View style={styles.formGroup}>
-                <Text style={styles.label}>Price</Text>
-                <TextInput
-                  style={styles.input}
-                  value={price}
-                  onChangeText={(text) => setPrice(text)}
-                />
-              </View>
-              <View style={styles.formGroup}>
-                <Text style={styles.label}>Description</Text>
-                <TextInput
-                  style={[styles.input, { height: 80 }]}
-                  value={description}
-                  onChangeText={(text) => setDescription(text)}
-                />
-              </View>
-
-              <TouchableOpacity
-                style={styles.uploadContainer}
-                onPress={pickImage}
-              >
-                {image ? (
-                  <Image source={{ uri: image }} style={styles.uploadedImage} />
-                ) : (
-                  <View style={styles.uploadContent}>
-                    <Image
-                      source={{ uri: "https://example.com/upload-icon.png" }} // Replace with your upload icon URL
-                      style={styles.icon}
-                    />
-                    <Text style={styles.uploadText}>upload</Text>
-                  </View>
-                )}
-              </TouchableOpacity>
-
-              <View style={styles.formGroup}>
-                <Text style={styles.label}>Size</Text>
-                <TextInput
-                  style={styles.input}
-                  value={size}
-                  onChangeText={(text) => setsize(text)}
-                />
-              </View>
-              <View style={styles.formGroup}>
-                <Text style={styles.label}>Flavours Available</Text>
-                <TextInput
-                  style={styles.input}
-                  value={flavour}
-                  onChangeText={(text) => setFlavour(text)}
-                />
-              </View>
+      <ScrollView style={styles.container}>
+        <Text style={{ fontSize: 32, fontWeight: "700" }}>Add Cake</Text>
+        <View style={{ flexDirection: "column", gap: 50 }}>
+          <View style={{ marginTop: 10, gap: 15 }}>
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Name of Cake</Text>
+              <TextInput
+                style={styles.input}
+                value={cakeName}
+                onChangeText={(text) => setCakeName(text)}
+              />
+            </View>
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Price</Text>
+              <TextInput
+                style={styles.input}
+                value={price}
+                onChangeText={(text) => setPrice(text)}
+              />
+            </View>
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Description</Text>
+              <TextInput
+                style={[styles.input, { height: 80 }]}
+                value={description}
+                onChangeText={(text) => setDescription(text)}
+              />
             </View>
 
-            <View>
-              <Pressable style={styles.button} onPress={uploadProductHandler}>
+            <TouchableOpacity
+              style={styles.uploadContainer}
+              onPress={pickImage}
+            >
+              {image ? (
+                <Image source={{ uri: image }} style={styles.uploadedImage} />
+              ) : (
+                <View style={styles.uploadContent}>
+                  <Image
+                    source={{ uri: "https://example.com/upload-icon.png" }} // Replace with your upload icon URL
+                    style={styles.icon}
+                  />
+                  <Text style={styles.uploadText}>upload</Text>
+                </View>
+              )}
+            </TouchableOpacity>
+
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Size</Text>
+              <TextInput
+                style={styles.input}
+                value={size}
+                onChangeText={(text) => setsize(text)}
+              />
+            </View>
+
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                paddingHorizontal: 20,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 18,
+                  marginBottom: 10,
+                }}
+              >
+                Select Status:
+              </Text>
+            </View>
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Flavours Available</Text>
+              <TextInput
+                style={styles.input}
+                value={flavour}
+                onChangeText={(text) => setFlavour(text)}
+              />
+            </View>
+
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                paddingHorizontal: 20,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 18,
+                  marginBottom: 10,
+                }}
+              >
+                Select Status:
+              </Text>
+              <TouchableOpacity
+                style={{
+                  borderWidth: 1,
+                  borderColor: "#ccc",
+                  padding: 10,
+                  borderRadius: 5,
+                }}
+                onPress={() => setIsModalVisible(true)}
+              >
                 <Text
                   style={{
-                    textAlign: "center",
-                    color: "white",
                     fontSize: 16,
-                    fontWeight: "400",
+                    textTransform: "lowercase",
                   }}
                 >
-                  Preview
+                  {selectedStatus}
                 </Text>
-              </Pressable>
+              </TouchableOpacity>
+
+              <Modal
+                visible={isModalVisible}
+                transparent={true}
+                animationType="slide"
+              >
+                <View
+                  style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: "rgba(0, 0, 0, 0.5)",
+                  }}
+                >
+                  <View
+                    style={{
+                      backgroundColor: "white",
+                      width: "80%",
+                      borderRadius: 10,
+                      padding: 20,
+                    }}
+                  >
+                    <FlatList
+                      data={statuses}
+                      keyExtractor={(item) => item}
+                      renderItem={({ item }) => (
+                        <TouchableOpacity
+                          style={{
+                            paddingVertical: 10,
+                          }}
+                          onPress={() => handleStatusSelect(item)}
+                        >
+                          <Text
+                            style={{
+                              fontSize: 18,
+                              textTransform: "lowercase",
+                            }}
+                          >
+                            {item}
+                          </Text>
+                        </TouchableOpacity>
+                      )}
+                    />
+                  </View>
+                </View>
+              </Modal>
             </View>
           </View>
+
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#DD293E",
+              padding: 10,
+              borderRadius: 20,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                fontSize: 16,
+                fontWeight: "400",
+                color: "white",
+              }}
+            >
+              Preview
+            </Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </AppScreenTwo>
@@ -151,8 +269,9 @@ export default UploadProduct;
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
-    top: 100,
+    flex: 1,
+    padding: 10,
+    marginTop: 80,
   },
   formGroup: {
     flexDirection: "column",
