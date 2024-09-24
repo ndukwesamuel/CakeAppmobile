64e53dadaa5acea7ddc63b7e54767321f471d187
Eniola <opakunleeniolail2019@gmail.com>
2024-09-24 10:02:17 +0100

    style: worked on categoryCakes

---
+++ screens/Buyer/Home.js/AdditionalInfo.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import AppScreenThree from '../../../components/shared/AppScreenThree'
+
+export default function AdditionalInfo() {
+  return (
+    <AppScreenThree arrrow={"true"} title={"Additional Information"}>
+        <View>
+            
+        </View>
+
+    </AppScreenThree>
+  )
+}
+
+const styles = StyleSheet.create({})
--- screens/Buyer/Home.js/CategoryCakes.js
+++ screens/Buyer/Home.js/CategoryCakes.js
@@ -1,9 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import AppScreen from "../../../components/shared/AppScreen";
 import AppScreenTwo from "../../../components/shared/AppScreenTwo";
 import AppScreenThree from "../../../components/shared/AppScreenThree";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { useDispatch, useSelector } from "react-redux";
 import { Get_All_Cake_Fun } from "../../../Redux/Buyer/CakeSlice";
 
@@ -11,18 +19,30 @@ export default function CategoryCakes() {
   const dispatch = useDispatch();
   const { get_all_cake_data } = useSelector((state) => state.CakeSlice);
   const dataRoute = useRoute()?.params;
-  const [option, setoption] = useState('')
-  console.log({ route: dataRoute });
+  const [option, setoption] = useState("");
+  //   console.log({ route: dataRoute });
 
   useEffect(() => {
-    setoption(dataRoute?.name)
-    dispatch(Get_All_Cake_Fun(option))
+    setoption(dataRoute?.item?.name);
+    dispatch(Get_All_Cake_Fun(dataRoute?.item?.name));
     return () => {};
   }, [dataRoute]);
-  console.log({data:get_all_cake_data?.data?.cakes})
+  console.log({ option: option });
+  console.log({ data: get_all_cake_data?.data?.cakes });
   return (
     <AppScreenThree arrrow={"true"} title={dataRoute?.item?.name}>
-      <View style={styles.container} id="container"></View>
+      <View style={styles.container} id="container">
+        <FlatList
+          data={get_all_cake_data?.data?.cakes}
+          renderItem={({ item }) => <ImageCard item={item} />}
+          keyExtractor={(item) => item._id}
+          ListEmptyComponent={
+            <View>
+              <Text>No cakes under this category </Text>
+            </View>
+          }
+        />
+      </View>
     </AppScreenThree>
   );
 }
@@ -32,5 +52,78 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 20,
     marginTop: 70,
+  },
+});
+
+const ImageCard = ({ item }) => {
+  const navigation = useNavigation();
+  return (
+    <TouchableOpacity style={styles2.container}>
+      <Image source={{ uri: item.images[0].url }} style={styles2.image} />
+      <View style={{ flexDirection: "column", gap: 20, padding: 15 }}>
+        <Text style={styles2.cardTitle}>{item?.name}</Text>
+        <Text style={styles2.description}>{item.description}</Text>
+        <Text style={styles2.cardTitle}>{item.price}</Text>
+        <Pressable
+          style={styles2.button}
+          onPress={() => navigation.navigate("cakeDetails")}
+        >
+          <Text
+            style={{
+              color: "white",
+              fontSize: 14,
+              fontWeight: "500",
+              textAlign: "center",
+            }}
+          >
+            Place Order
+          </Text>
+        </Pressable>
+        <Pressable>
+          <Text
+            style={{
+              textAlign: "center",
+              textDecorationLine: "underline",
+              fontWeight: "500",
+              fontSize: 14,
+              color: "#2B025F",
+            }}
+          >
+            Add to Wishlist
+          </Text>
+        </Pressable>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles2 = StyleSheet.create({
+  container: {
+    borderWidth: 0.5,
+    borderColor: "#00000033",
+    marginBottom: 10,
+    borderRadius: 12,
+  },
+  image: {
+    width: "100%",
+    height: 180,
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12,
+  },
+  cardTitle: {
+    color: "#2B025F",
+    fontSize: 16,
+    fontWeight: "700",
+  },
+  description: {
+    color: "#2B025F",
+    fontSize: 12,
+    fontWeight: "400",
+  },
+  button: {
+    backgroundColor: "#6904EC",
+    borderRadius: 30,
+    paddingHorizontal: 37,
+    paddingVertical: 15,
   },
 });
