072c519ca8cd96773460c48161d0b184abe11483
Eniola <opakunleeniolail2019@gmail.com>
2024-10-04 16:14:08 +0100

    feat: worked on ongoing modal

--- screens/Vendor/Modals/OngoingModal.js
+++ screens/Vendor/Modals/OngoingModal.js
@@ -3,10 +3,11 @@ import React, { useState } from "react";
 import { formatDate } from "../../../utills/DateTime";
 import { useMutation } from "react-query";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 export default function OngoingModal({ item }) {
   const [openModal, setOpenModal] = useState(false);
-  const [openDeliveredModal, setOpenDeliveredModal] = useState(false)
+  const [openDeliveredModal, setOpenDeliveredModal] = useState(false);
   const token = useSelector((state) => state?.Auth?.user_data?.data?.token);
 
   const updateOrder_Mutation = useMutation(
@@ -33,7 +34,7 @@ export default function OngoingModal({ item }) {
         //   type: "success",
         //   text1: `${success?.data?.message}`,
         // });
-        setOpenDeliveredModal(!openDeliveredModal)
+        setOpenDeliveredModal(!openDeliveredModal);
       },
       onError: (error) => {
         console.log(error);
@@ -49,7 +50,7 @@ export default function OngoingModal({ item }) {
     const formData = {
       status: "completed",
     };
-    updateOrder_Mutation.mutate({formData, token})
+    updateOrder_Mutation.mutate({ formData, token });
   };
   return (
     <View style={styles.container2}>
@@ -115,10 +116,7 @@ export default function OngoingModal({ item }) {
                   <Text>{item?.address}</Text>
                 </View>
               </View>
-              <Pressable
-                style={styles.button}
-                onPress={handleMarkAsDelivered}
-              >
+              <Pressable style={styles.button} onPress={handleMarkAsDelivered}>
                 <Text
                   style={{
                     textAlign: "center",
@@ -134,8 +132,54 @@ export default function OngoingModal({ item }) {
           </View>
         </View>
       </Modal>
-      <Modal visible={openDeliveredModal}>
-
+      <Modal
+        visible={openDeliveredModal}
+        transparent={true}
+        animationType="slide"
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContent}>
+            {/* Modal Content */}
+            <View
+              style={{
+                backgroundColor: "white",
+                padding: 20,
+                width: "100%",
+                gap: 10,
+              }}
+            >
+              <Text
+                style={{
+                  textAlign: "center",
+                  color: "#2B025F",
+                  fontSize: 24,
+                  fontWeight: "600",
+                }}
+              >
+                Marked Delivered
+              </Text>
+              <Text
+                style={{
+                  textAlign: "center",
+                  color: "#2B025F",
+                  fontSize: 14,
+                  fontWeight: "400",
+                }}
+              >
+                Your order has been marked delivered, we await buyers
+                confirmation
+              </Text>
+              <Pressable
+                style={[styles.button, { backgroundColor: "#6904EC" }]}
+                onPress={() => setOpenDeliveredModal(!openDeliveredModal)}
+              >
+                <Text style={{ textAlign: "center", color: "white" }}>
+                  Okay
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
       </Modal>
     </View>
   );
